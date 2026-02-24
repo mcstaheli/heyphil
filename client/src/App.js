2df0199d16f01643198be92cf0fc0f493f019b74
Philo <philo@chads-mac-mini.tail95a4d0.ts.net>
2026-02-24 10:58:21 -0700

    Change 'Closed' column title to 'Packaged'

--- client/src/App.js
+++ client/src/App.js
@@ -181,7 +181,7 @@ function OriginationBoard({ user, onBack, onLogout }) {
     { id: 'ic-capitalization', title: 'IC - Capitalization', color: '#bdbdbd' },
     { id: 'capitalization', title: 'Capitalization', color: '#42a5f5' },
     { id: 'ic-close', title: 'IC - Close', color: '#9e9e9e' },
-    { id: 'closed', title: 'Closed', color: '#2196f3' },
+    { id: 'closed', title: 'Packaged', color: '#2196f3' },
     { id: 'abandoned', title: 'Abandoned', color: '#757575' }
   ];
 
